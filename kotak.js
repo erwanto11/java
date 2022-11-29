997b1fd50b8e3d629e05c0f6517400e6a1df916d
erwanto11 <66783499+erwanto11@users.noreply.github.com>
2022-11-29 18:16:16 +0700

    Update kotak.js

--- kotak.js
+++ kotak.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 	atOptions = {
 		'key' : '3c174b6005373ceaffae0f0c2730a7ca',
 		'format' : 'iframe',
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bangedavenge.com/3c174b6005373ceaffae0f0c2730a7ca/invoke.js"></scr' + 'ipt>');
-</script>
