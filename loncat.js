c0135cc2e59b662eb592d9f16a7dfbea30a8c8b7
erwanto11 <66783499+erwanto11@users.noreply.github.com>
2022-11-29 18:47:22 +0700

    Create loncat.js

---
+++ loncat.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://bangedavenge.com/xw8uh4j56?key=e09217a08ffa1bf731fa7fd389dcc874', '_blank');
+	});
