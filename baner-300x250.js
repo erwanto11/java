02794f20b72495dfbbdd23fa431f08ffeba3f8a2
erwanto11 <66783499+erwanto11@users.noreply.github.com>
2022-12-02 21:22:19 +0700

    Update and rename shopee.js to baner-300x250.js

---
+++ baner-300x250.js
@@ -0,0 +1,52 @@
+<!-- Begin
+rnd.today=new Date();
+rnd.seed=rnd.today.getTime();
+
+function rnd() {
+rnd.seed = (rnd.seed*9301+49297) % 233280;
+return rnd.seed/(233280.0);
+};
+
+function rand(number) {
+var result = Math.ceil(rnd()*number);
+if (!result)result++;
+return result
+};
+var ad_cnt1 = 4;
+var ad1 = rand(ad_cnt1);
+var link1;
+var adBanner1;
+var width1
+var height1
+if (ad1==1) {
+link1="https://shope.ee/8pCDgmcIj3";
+adBanner1="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhsY3pdFSaaYBE8tB6pxufg6vsHOxELfRLXPbhisdmEwSSvZdYrO43wh2Nx0FWv91eylhjezjH6ZJlRKjAxM3oNfGiNFyIKzZOMmS_AQ2LhZXXxbHRABmX0Q3mbXCrTAlYXV03UgUC976k94a176Z2go-VjY9_LkaZKLh7KtOKjGFaXskfpMQusDFjf/s16000/shopee1.webp";
+width1="300";
+height1="250";
+alt1="beli sepatu di shopee";
+}
+if (ad1==2) {
+link1="https://shope.ee/5KcLVMf4IX";
+adBanner1="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjqw8dK9OORuEyH2tF01Cz0dA5LlnIguYLVIx8_pf_7ArSyLVNB4AuyxsfE1c8gasbUULhJA4D7tTtB_iMho_Lbm9sqqh1Ko9EMQ39B6gq0h2ewsYvs2B5UZga2mYg9sDmxEDQIFOcokcS-e_N8ysMt0lmiCGUnELtww55Fw9rK77Qy0W84w9DD_TEO/s1600/shopee2.webp";
+width1="300";
+height1="250";
+alt1="beli sepatu keren di shopee";
+}
+if (ad1==3) {
+link1="https://tokopedia.link/4qwImlDOqvb";
+adBanner1="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhS7p4jrLcejQx_uhOU830ZhJ9DdBNHi8Pq2GmUs0MKDOlpgDpU6GbfygJWfYDeugS5o5cXaEknrwNJcl2fdyzTqMDTLhuGpZYtoE2AlbAPUgbrJHVcI1CMlqu22DHt1FnMUChEltvvHp6R9DEffFdeUdGxZp-RQcE-0fZGt72oDNt6vg0gWcOBytVg/s1600/tokopedia-1.webp";
+width1="300";
+height1="250";
+alt1="beli sepatu di tokopedia";
+}
+if (ad1==4) {
+link1="https://tokopedia.link/t9pUs6HOqvb";
+adBanner1="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEimI9DzfM7yF4H8SOThTx0Qa4KKWJOB4dWZL50GErD7PLWTG2LcHErXGwVhZ0d9VyoRyXuFanoYZAi-X4Ro4cZNZS9-RrKNxLB5JqQQKcEBy_OZCMg690sYIdisgXZV0gL1P8YXE8Rx1A58UVeE9a4BlH8hh9_G-9CGSUMnJna7D0n_PpsLRx-yjDCk/s1600/tokopedia-2.webp";
+width1="300";
+height1="250";
+alt1="beli sepatu keren di tokopedia";
+}
+document.write('<center><a href="' + link1 + '" target="_blank">');
+document.write('<img src="' + adBanner1 + '" width=' + width1 + ' height=' + height1 + ' border=0 alt="' + alt1 + '"></a>');
+document.write('</center>');
+// End -->
