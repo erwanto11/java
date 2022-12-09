844807cef8d7a40da92f03214e2f8bb389f711ce
erwanto11 <66783499+erwanto11@users.noreply.github.com>
2022-12-09 22:50:48 +0700

    Create adsterra-300x250.js

---
+++ adsterra-300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '3c174b6005373ceaffae0f0c2730a7ca',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bangedavenge.com/3c174b6005373ceaffae0f0c2730a7ca/invoke.js"></scr' + 'ipt>')
