2c830cbefd96764b2e3a80749f8a408a2788f921
erwanto11 <66783499+erwanto11@users.noreply.github.com>
2024-05-21 17:41:42 +0700

    Update adsterra-300x250.js

--- adsterra-300x250.js
+++ adsterra-300x250.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '3c174b6005373ceaffae0f0c2730a7ca',
+		'key' : '562b91f3b09d7f1553f1f2a9cbac00f2',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bangedavenge.com/3c174b6005373ceaffae0f0c2730a7ca/invoke.js"></scr' + 'ipt>')
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bangedavenge.com/562b91f3b09d7f1553f1f2a9cbac00f2/invoke.js"></scr' + 'ipt>')
